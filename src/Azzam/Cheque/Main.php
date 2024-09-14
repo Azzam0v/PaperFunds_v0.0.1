cba2b629250263aa4acc621a9345782f270423e4
Azzam0v <kettani.azzam@gmail.com>
2024-09-14 11:20:07 -0300

    Ajout d'une config

--- src/Azzam/Cheque/Main.php
+++ src/Azzam/Cheque/Main.php
@@ -8,29 +8,42 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerItemUseEvent;
+use pocketmine\item\LegacyStringToItemParser;
+use pocketmine\item\StringToItemParser;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\item\ItemTypeIds;
 use pocketmine\item\VanillaItems;
+use pocketmine\utils\Config;
 use Terpz710\EconomyPE\Money;
 
 class Main extends PluginBase implements Listener
 {
     private $cooldown = [];
+    public $valeur_max;
+    public $valeur_min;
+    public $delai;
+    public $config;
 
     public function OnEnable(): void
     {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveResource('config.yml');
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $this->valeur_max = $this->config->get("Valeur_max", 100000);
+        $this->delai = $this->config->get("Delai", 30);
+        $this->valeur_min = $this->config->get("Valeur_min", 1000);
     }
 
     public function Interact(PlayerItemUseEvent $event){
         $item = $event->getItem();
         $player = $event->getPlayer();
-        if($item->getTypeId() === ItemTypeIds::PAPER) {
+        $itemId = $this->config->get("Item_ID", "paper");
+        if($item === StringToItemParser::getInstance()->parse($itemId) ?? LegacyStringToItemParser::getInstance()->parse($itemId)) {
             if ($item->hasCustomName()){
                 if (!isset($this->cooldown[$player->getName()]))
                 {
-                    $this->cooldown[$player->getName()] = time() + 30; //pour éviter tout usebug avec un bug de connexion
+                    $this->cooldown[$player->getName()] = time() + $this->delai; //pour éviter tout usebug avec un bug de connexion
                     $lore = $item->getLore();
                     if(isset($lore[0])){
                         if(intval($lore[0])){
@@ -99,7 +112,7 @@ class Main extends PluginBase implements Listener
                 $player->sendMessage("§9>> §fVous devez mettre uniquement des chiffres !");
             }
         });
-        $form->setTitle("Chèque");
+        $form->setTitle($this->config->get("Titre", "Chèque"));
         $form->addLabel("§9>> §fBienvenue sur l'interface du §9Chéquier§f ! Il vous permettra de créer des chèques qui pourront vous servir.");
         $form->addInput("§9>> §fVous devez mettre uniquement des chiffres !", "Entrez le montant du chèque");
 
@@ -108,9 +121,10 @@ class Main extends PluginBase implements Listener
     }
 
     public function onUseCheque($args, $sender){
-        $item = VanillaItems::PAPER()->setCount(1);
-        if ($args[0] > 999){
-            if ($args[0] < 100000){
+        $itemId = $this->config->get("Item_ID", "paper");
+        $item = StringToItemParser::getInstance()->parse($itemId)->setCount(1) ?? LegacyStringToItemParser::getInstance()->parse($itemId)->setCount(1);
+        if ($args[0] >= $this->valeur_min){
+            if ($args[0] <= $this->valeur_max){
                 $_price = round($args[0]);
                 if (Money::getInstance()->getMoneyPlayer($sender) < $_price){
                     $sender->sendMessage("§cVous n'avez pas les fonds suffisants !");
@@ -118,15 +132,15 @@ class Main extends PluginBase implements Listener
                 }
                 Money::getInstance()->removeMoney($sender, $args[0]);
 
-                $item->setCustomName("§fChèque de §9$args[0]$");
+                $item->setCustomName("§f".$this->config->get("Titre")." de §9$args[0]$");
                 $item->setLore([$args[0]]);
                 $sender->getInventory()->addItem($item);
                 $sender->sendMessage("§9>> §fVous venez de créer un chèque de §9". $args[0] ."$ §f!");
             }else{
-                $sender->sendMessage("§9>> §fLa somme maximal est de §9100'000$ §f!");
+                $sender->sendMessage("§9>> §fLa somme maximal est de §9".$this->valeur_max." §f!");
             }
         }else{
-            $sender->sendMessage("§9>> §fLa somme minimal est de §91000$ §f!");
+            $sender->sendMessage("§9>> §fLa somme minimal est de §9".$this->valeur_min." §f!");
         }
     }
 
